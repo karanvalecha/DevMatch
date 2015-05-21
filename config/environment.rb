1e0f62635663ad14c361ed94788747db97a2809e
Karan Valecha <karan.valecha92@gmail.com>
2015-05-21 21:15:09 +0530

    Configured for sendgrid starter mailing service

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,12 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+ActionMailer::Base.smtp_settings = {
+	:address => 'smtp.sendgrid.net',
+	:port =>  '587',
+	:authentication => 'plain',
+	:username => ENV['SENDGRID_USERNAME'],
+	:password => ENV['SENDGRID_PASSWORD'],
+	:domain  => 'heroku.com',
+	:enable_startstls_aotu => true
+}
