7ce8f67d28f25c4721a0b4b9a53a9fec7454f8d2
Karan Valecha <karan.valecha92@gmail.com>
2015-05-29 22:49:55 +0530

    Added before filters to check valid users

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,7 @@
 class ProfilesController < ApplicationController
+	before_action :authenticate_user!
+	before_action :only_current_user
+
 	def new
 		#form where user can fill out there own profile
 		@user = User.find( params[:user_id] )
@@ -21,9 +24,25 @@ class ProfilesController < ApplicationController
 		@profile = @user.profile
 	end
 
+	def update
+		@user = User.find( params[:user_id] )
+		@profile = @user.profile
+		if @profile.update_attributes(profile_params)
+			flash[:success] = "Profile Updated"
+			redirect_to user_path( params[:user_id] )
+		else
+			render action: :edit
+		end
+	end
+
 	private
 	  def profile_params
 		params.require(:profile).permit( :first_name, :last_name, :job_title, :phone_number, :contact_email, :description )
 	  end
 
+	  def only_current_user
+	  	@user = User.find( params[:user_id] )
+	  	flash[:warning] = "Hey, You are'nt allowed to touch other's profile"
+	  	redirect_to(root_url) unless @user == current_user
+	  	end
 end
